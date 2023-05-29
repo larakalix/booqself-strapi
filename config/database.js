1e0651de197b9f6273c8ea19bf64c2809a31fff8
Ivan Lara <ukikalix@gmail.com>
2023-05-29 19:19:26 -0400

    remove plugins from package.json

--- config/database.js
+++ config/database.js
@@ -1,25 +1,25 @@
-module.exports = ({ env }) => ({
-  connection: {
-    client: "postgres",
-    connection: {
-      host: env("PGHOST", "127.0.0.1"),
-      port: env.int("PGPORT", 5432),
-      database: env("PGDATABASE", "strapi"),
-      user: env("PGUSER", "strapi"),
-      password: env("PGPASSWORD", "password"),
-      ssl: env.bool(true),
-    },
-    pool: { min: 0 },
-  },
-});
-// const path = require("path");
-
-// module.exports = () => ({
+// module.exports = ({ env }) => ({
 //   connection: {
-//     client: "sqlite",
+//     client: "postgres",
 //     connection: {
-//       filename: path.join(__dirname, "..", ".tmp/data.db"),
+//       host: env("PGHOST", "127.0.0.1"),
+//       port: env.int("PGPORT", 5432),
+//       database: env("PGDATABASE", "strapi"),
+//       user: env("PGUSER", "strapi"),
+//       password: env("PGPASSWORD", "password"),
+//       ssl: env.bool(true),
 //     },
-//     useNullAsDefault: true,
+//     pool: { min: 0 },
 //   },
 // });
+const path = require("path");
+
+module.exports = () => ({
+  connection: {
+    client: "sqlite",
+    connection: {
+      filename: path.join(__dirname, "..", ".tmp/data.db"),
+    },
+    useNullAsDefault: true,
+  },
+});
