22c61a8b66f1a5f06544aa95e7079a6bb8d30a89
Ivan Lara <ukikalix@gmail.com>
2023-05-29 23:18:23 -0400

    Delete contract schema

--- src/api/contract/controllers/contract.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * contract controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::contract.contract');
