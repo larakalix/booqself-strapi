01f012892d45afdbba1d9c068662d517d0273f4b
Ivan Lara <ukikalix@gmail.com>
2023-05-29 21:36:46 -0400

    Add client schema to tenant

--- config/plugins.js
+++ config/plugins.js
@@ -1,21 +1,27 @@
 module.exports = ({ env }) => ({
-    'users-permissions': {
-      config: {
-      jwtSecret: env('JWT_SECRET'),
+  "users-permissions": {
+    config: {
+      jwtSecret: env("JWT_SECRET"),
+    },
+  },
+  upload: {
+    config: {
+      playgroundAlways: false,
+      defaultLimit: 10,
+      maxLimit: 20,
+      apolloServer: {
+        tracing: true,
+      },
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
       },
     },
-    upload: {
-      config: {
-        provider: 'cloudinary',
-        providerOptions: {
-          cloud_name: env('CLOUDINARY_NAME'),
-          api_key: env('CLOUDINARY_KEY'),
-          api_secret: env('CLOUDINARY_SECRET'),
-        },
-        actionOptions: {
-          upload: {},
-          delete: {},
-        },
-      },
-    },
-  });
+  },
+});
